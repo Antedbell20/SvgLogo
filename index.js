db2ad0c7f90b16a416f8d3cf59b984f6be897963
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-10-23 22:04:40 -0500

    added svg class

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
+const {circle, square, triangle} = require("./lib/shape");
 
-
+//adding questions 
 const questions = [
     {
         message: "enter your text (up to 3 characters",
@@ -18,9 +19,27 @@ const questions = [
         type: "input"
     },
     {
+        // making a list of choices
         message: "What shape do you want?",
         choices: ["circle", "square", "triangle"],
         name: "shape",
         type: "list"
     }
 ];
+//making svg class
+class svg {
+    constructor(){
+        this.textEl = ""
+        this.shapeEl = ""
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeEl}${this.textEl}</svg>`
+
+    }
+    setTextEl(text,color){
+        this.textEl = '<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>'
+    }
+    setShapeEl(shape){
+        this.shapeEl = shape.render()
+    }
+}
